f60ce5bfd968c44fa0b397b3ae1e48b1d1425ff9
Philip Cutler <cutlervufamily@gmail.com>
2023-09-11 23:05:40 +0930

    ensure pip is installed

--- run_ansible-install.sh
+++ run_ansible-install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install python3-pip
+
+wget https://bootstrap.pypa.io/get-pip.py
+
+python3 get-pip.py
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user pipx
